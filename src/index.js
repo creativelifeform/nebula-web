53a700d947b8827734b7fe3c9f9c281ee32d14d3
Rohan Deshpande <rohan@creativelifeform.com>
2019-07-11 23:10:15 +1000

    React-snap not working :(

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import * as serviceWorker from './serviceWorker';
 
+import { hydrate, render } from 'react-dom';
+
 import App from './App';
 import React from 'react';
-import { render } from 'react-dom';
 
-render(<App />, document.getElementById('root'));
+const rootNode = document.getElementById('root');
+
+if (rootNode.hasChildNodes()) {
+  hydrate(<App />, rootNode);
+} else {
+  render(<App />, rootNode);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
