d3f968d62940f51fc8cda69136885c4d3229379b
Rohan Deshpande <rohan@creativelifeform.com>
2019-10-20 14:38:48 +1100

    Reverted as this seems to make zero difference in remote

--- components/Home/Hero/index.js
+++ components/Home/Hero/index.js
@@ -1,11 +1,9 @@
 import { Content, Grid } from '../../primitives';
 
 import Blurb from './Blurb';
+import Canvas from './Canvas';
 import React from 'react';
-import dynamic from 'next/dynamic';
-
-const Canvas = dynamic(() => import('./Canvas'));
-const Video = dynamic(() => import('./Video'));
+import Video from './Video';
 
 export default () => (
   <Content className="Hero">
