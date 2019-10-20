44689239fd792681215baab1cc3ca147bb39930c
Rohan Deshpande <rohan@creativelifeform.com>
2019-10-20 15:30:24 +1100

    Fixed error being caused by the download form's error component being rendered as a sibling without a parent next to the AnalyticsConsumer

--- common/api/index.js
+++ common/api/index.js
@@ -1,13 +1,10 @@
-import { TEST_EMAIL, __PROD__ } from '../config';
-
 import Fetch from 'rd-fetch';
 import { SIGNUP } from './endpoints';
 
 export default class Api {
   static sendSignupRequest(body) {
     const method = 'POST';
-    const email = __PROD__ ? body.email : TEST_EMAIL;
 
-    return Fetch.json(SIGNUP, { method, body: { ...body, email } });
+    return Fetch.json(SIGNUP, { method, body });
   }
 }
