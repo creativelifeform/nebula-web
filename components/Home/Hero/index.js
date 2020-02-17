2df19206cd4877fa7f23cdc748a296ca46720637
Rohan Deshpande <rohan@creativelifeform.com>
2020-02-18 08:35:22 +1100

    Added responsive gif square to hero and made about media items squares

--- components/Home/Hero/index.js
+++ components/Home/Hero/index.js
@@ -2,6 +2,7 @@ import { Content, Grid } from '../../primitives';
 
 import Blurb from './Blurb';
 import Canvas from './Canvas';
+import Gif from './Gif';
 import React from 'react';
 import Video from './Video';
 
@@ -9,7 +10,10 @@ export default () => (
   <Content className="Hero">
     <Canvas />
     <Grid>
+      {/* for mobile */}
+      <Gif />
       <Blurb />
+      {/* for tablet and desktop */}
       <Video />
     </Grid>
   </Content>
