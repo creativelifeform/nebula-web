59b115da3e9ca38743d43a5505bab5a69ae478ea
Rohan Deshpande <rohan@creativelifeform.com>
2020-06-24 10:40:29 +1000

    Added react-use to adaptively load the right video based on screen size and network speed (#32)

--- components/Home/Hero/Video/Large.js
+++ components/Home/Hero/Video/Large.js
@@ -6,6 +6,6 @@ import src from '../../../../assets/hero-video-720p-60fps.mp4';
 /**
  * A video is displayed for medium-large viewports.
  */
-const Large = () => <Video src={src} className="VideoLarge" poster={poster} />;
+const Large = () => <Video src={src} className="Video large" poster={poster} />;
 
 export default Large;
--- components/Home/Hero/index.js
+++ components/Home/Hero/index.js
@@ -1,21 +1,29 @@
 import { Content, Grid } from '../../primitives';
 import { VideoLarge, VideoSmall } from './Video/async';
+import { useMedia, useNetwork } from 'react-use';
 
 import Blurb from './Blurb';
 import { Canvas } from './Canvas/async';
 import React from 'react';
 
-const Hero = () => (
-  <Content className="Hero">
-    <Canvas />
-    <Grid>
-      {/* for mobile */}
-      <VideoSmall />
-      <Blurb />
-      {/* for tablet and desktop */}
-      <VideoLarge />
-    </Grid>
-  </Content>
-);
+const Video = () => {
+  const isWide = useMedia('(min-width: 45em)');
+  const { effectiveType: connection } = useNetwork();
+  const shouldRenderSmallVideo = !isWide || connection !== '4g';
+
+  return shouldRenderSmallVideo ? <VideoSmall /> : <VideoLarge />;
+};
+
+const Hero = () => {
+  return (
+    <Content className="Hero">
+      <Canvas />
+      <Grid>
+        <Blurb />
+        <Video />
+      </Grid>
+    </Content>
+  );
+};
 
 export default Hero;
