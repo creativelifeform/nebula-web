053ffeb9083f9523add05ce27fec565c76354dbb
Rohan Deshpande <rohan@creativelifeform.com>
2020-06-28 17:28:38 +1000

    Styling a little better, we have two sections now

--- pages/guides/installation/windows/index.js
+++ pages/guides/installation/windows/index.js
@@ -1,4 +1,8 @@
 import { Guides } from '../../../../components';
 import React from 'react';
 
-export default () => <Guides />;
+export default () => (
+  <Guides>
+    <div className="Guide">This is the Windows installation guide</div>
+  </Guides>
+);
